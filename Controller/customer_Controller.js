df7d587d331b8dcffd890c316ae7222d8e9647f4
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2024-08-21 05:09:37 +0530

    Convert BackendAAD

--- Controller/customer_Controller.js
+++ Controller/customer_Controller.js
@@ -4,19 +4,38 @@ var recordIndex;
 import {addOptionToDropdown} from "./order_controller.js";
 
 $("#addBtn").on('click', () => {
- var id = $("#inputCusID").val();
- var name = $("#NameCus").val();
- var address = $("#inputAddressCus").val();
- var number = $("#inputAddress2Cus").val();
+ var id1 = $("#inputCusID").val();
+ var name1 = $("#NameCus").val();
+ var address1 = $("#inputAddressCus").val();
+ var number1 = $("#inputAddress2Cus").val();
 
- var Cusmodel = new CustomerModel(id, name, address, number);
+ var Cusmodel = new CustomerModel(id1, name1, address1, number1);
 
- if (!(id === '' || name === '' || address === '' || number === '')) {
-  var valid = validation(id,name,address,number)
+ if (!(id1 === '' || name1 === '' || address1 === '' || number1 === '')) {
+  var valid = validation(id1,name1,address1,number1)
   if (valid==true) {
-   Savearr.push(Cusmodel);
-   addOptionToDropdown(Cusmodel.id);
-   loadTable();
+
+$.ajax({
+ url:"http://localhost:8085/JNDI/customer",
+ method:"POST",
+ contentType:"application/json",
+ "data":JSON.stringify({
+  id:id1,
+  name:name1,
+  address:address1,
+  number:number1
+}),
+ success:function(results){
+  console.log(results);
+
+ },
+ error:function(error){
+  console.log(error);
+
+ }
+});
+  // addOptionToDropdown(Cusmodel.id);
+  loadTable();
    $("#reset").click()
   }
  }else {
@@ -36,20 +55,49 @@ $("#addBtn").on('click', () => {
   }
  }
   function loadTable() {
-   console.log(Savearr);
-   $("#cusTablebody").empty();
+   //console.log(Savearr);
+   $.ajax({
+    url: "http://localhost:8085/JNDI/customer",
+    method: "GET",
+    success: function(results) {
+     // Log the entire response as a formatted JSON string
+     console.log("Response Data:", JSON.stringify(results, null, 2));
 
-   Savearr.forEach(Item => {
-   var record = `
-            <tr>
-                <th scope="row" class="r1">${Item.id}</th>
-                <td class="r2">${Item.name}</td>
-                <td class="r3">${Item.address}</td>
-                <td class="r4">${Item.number}</td>
-            </tr>`;
-    $("#cusTablebody").append(record);
+     // Clear the table body before appending new data
+     $("#cusTablebody").empty();
+
+     // Check if the result is an array
+     if (Array.isArray(results)) {
+      results.forEach(function(customer, index) {
+       // Create a table row for each customer
+       var record = `
+                    <tr>
+                        <th scope="row" class="r1">${customer.id}</th>
+                        <td class="r2">${customer.name}</td>
+                        <td class="r3">${customer.address}</td>
+                        <td class="r4">${customer.number}</td>
+                    </tr>`;
+       // Append the row to the table body
+       $("#cusTablebody").append(record);
+      });
+     } else {
+      // If it's a single object (not an array), create a single row
+      var record = `
+                <tr>
+                    <th scope="row" class="r1">${results.id}</th>
+                    <td class="r2">${results.name}</td>
+                    <td class="r3">${results.address}</td>
+                    <td class="r4">${results.number}</td>
+                </tr>`;
+      $("#cusTablebody").append(record);
+     }
+    },
+    error: function(error) {
+     console.log("Error:", error);
+    }
    });
- }
+
+  }
  $("#reset").on('click', () => {
   $("#inputCusID").val('');
   $("#NameCus").val('');
